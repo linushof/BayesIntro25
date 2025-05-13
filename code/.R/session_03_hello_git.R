90e126c0e57f44a7be2931d0d2b74ee70ef4d319
Linus Hof <linus.hof@tum.de>
2025-05-13 16:26:21 +0200

    script for debugging

---
+++ code/.R/session_03_hello_git.R
@@ -0,0 +1,31 @@
+# load packages
+
+library(tidyverse)
+
+#read data
+
+dat <- read_csv('data/stocks.xlsx')
+
+# compute returns
+
+A_return <- (dat[nrow(dat),'stock_A']-dat[1,'stock_A'])/dat[1,'stock_A'] # 10-years 
+A_ann <- (1+A_return)^(1/10)-1 # annualized
+
+B_return <- (dat[nrow(dat),'stock_B']-dat[2,'stock_B'])/dat[2,'stock_B'] # 10-years 
+B_ann <- (1+B_return^(1/10))-1 # annualized
+
+
+# visualize stocks
+
+## pivot data into long format
+
+dat_long <- dat %>% 
+  pivot_longer(cols=stock_A:stock_B, names_to = 'Stock', values_to = 'Points')
+
+## plot stock development
+
+ggplot(dat_long, aes(x=year, y=Points, color=Stock)) +
+  geom_line(linewidth = 1) + 
+  labs(title='BLUE & RED Stock', 
+       subtitle='10 YR RETURN') +
+  theme_minimal()
